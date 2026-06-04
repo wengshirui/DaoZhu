ee155fe108910fec1cbb757f0aa9a4a913c979a9
wengshirui <yumen2278@163.com>
2026-06-04 16:51:57 +0800

    fix: hide CMD window + fix pet store corrupted emojis

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,6 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+// Prevents console window on Windows (both debug and release)
+#![windows_subsystem = "windows"]
 
 fn main() {
-  app_lib::run();
+    app_lib::run();
 }
